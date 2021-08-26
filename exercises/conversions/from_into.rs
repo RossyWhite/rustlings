46748c86782a8a37eb63852bb97f7a0666819102
RossyWhite <daikishiroi@gmail.com>
2021-08-26 12:53:39 +0900

    from_into

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -33,10 +33,31 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+
+        let split: Vec<&str> = s.split(",").collect();
+        if split.len() != 2 {
+            return Person::default();
+        }
+
+        let name = split[0].to_string();
+        if name == "" {
+            return Person::default();
+        }
+
+        let age_result = split[1].parse::<usize>();
+        match age_result {
+            Err(_) => Person::default(),
+            Ok(age) => Person {
+                name,
+                age,
+            }
+        }
     }
 }
 
@@ -52,6 +73,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
     fn test_default() {
         // Test that the default person is 30 year old John
@@ -59,6 +81,7 @@ mod tests {
         assert_eq!(dp.name, "John");
         assert_eq!(dp.age, 30);
     }
+
     #[test]
     fn test_bad_convert() {
         // Test that John is returned when bad string is provided
@@ -66,6 +89,7 @@ mod tests {
         assert_eq!(p.name, "John");
         assert_eq!(p.age, 30);
     }
+
     #[test]
     fn test_good_convert() {
         // Test that "Mark,20" works
@@ -73,6 +97,7 @@ mod tests {
         assert_eq!(p.name, "Mark");
         assert_eq!(p.age, 20);
     }
+
     #[test]
     fn test_bad_age() {
         // Test that "Mark,twenty" will return the default person due to an error in parsing age
